92777819d6d4fc154d2471e7a66d2c20b8e774bd
zjhch123 <416836508@qq.com>
2018-05-02 16:14:52 +0800

    finish

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import '@css/base.css';
 import '@css/style.scss';
+import data from './data.js';
 
 function start() {
+  if (document.querySelector('.J_modal').style.display == 'block') {
+    return
+  }
   const obj = document.querySelector('.J_shake_obj');
   if ([].slice.call(obj.classList).indexOf('shake') == -1) {
-    obj.classList.add('shake');
+    obj.classList.add('shake')
     setTimeout(() => {
-      obj.classList.remove('shake');
+      obj.classList.remove('shake')
+      if (Math.random() > 0.5) {
+        getResult()
+      } else {
+        alert('没有摇出签哦~请更诚心一些!')
+      }
     }, 1900);
   }
 }
 
+function getResult() {
+  const id = ~~(Math.random() * 1000 % 100)
+  const result = data[id]
+  document.querySelector('.J_result_title').innerHTML = result.title.trim()
+  document.querySelector('.J_result_word').innerHTML = result.word.replace('；', '；<br>').trim()
+  document.querySelector('.J_result_resolve').innerHTML = ''
+  document.querySelector('.J_result_mean').innerHTML = ''
+  document.querySelector('.J_result_get_resolve').setAttribute('data-id', id)
+  document.querySelector('.J_modal').style.display = 'block'
+}
+
+
 if (window.DeviceMotionEvent) {
-  var speed = 30; // 用来判定的加速度阈值，太大了则很难触发
+  var speed = 20; // 用来判定的加速度阈值，太大了则很难触发
   var x, y, z, lastX, lastY, lastZ;
   x = y = z = lastX = lastY = lastZ = 0;
 
@@ -31,3 +52,16 @@ if (window.DeviceMotionEvent) {
 } else {
   alert('您的设备不支持参加此活动');
 }
+
+document.querySelector('.J_result_get_resolve').addEventListener('click', function (event) {
+  document.querySelector('.J_result_resolve').innerHTML = data[this.getAttribute('data-id')].resolve.trim()
+  document.querySelector('.J_result_mean').innerHTML = data[this.getAttribute('data-id')].mean.trim()
+  this.style.display = 'none'
+  document.querySelector('.J_close').style.display = 'block'
+})
+
+document.querySelector('.J_close').addEventListener('click', function (event) {
+  document.querySelector('.J_modal').style.display = 'none'
+  this.style.display = 'none'
+  document.querySelector('.J_result_get_resolve').style.display = 'block'
+})
